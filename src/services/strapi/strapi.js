b6412d535144b46a693f987dbd91e44fe5a5399d
Andrey <andrey.sergienko.dev@gmail.com>
2023-02-12 15:17:35 +0300

    Added method for getting categories

--- src/services/strapi/strapi.js
+++ src/services/strapi/strapi.js
@@ -12,19 +12,22 @@ export class Strapi {
     return Promise.resolve(data.map(this.transformCard));
   }
 
-  transformCard = (book) => {
-    const resBook = {
-      id: book.id,
-      issueYear: book.issueYear,
-      rating: Math.round(book.rating),
-      categories: book.categories,
-      title: book.title,
-      authors: book.authors,
-      image: this.getImage(book.image?.url),
-      isBooked: true,
-    };
-    return resBook;
+  getCategories = async () => {
+    const response = await fetch(`${this.apiBase}/api/categories`);
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return Promise.resolve([{ name: 'Все книги', path: 'all' }, ...data]);
   };
+
+  transformCard = (book) => ({
+    ...book,
+    rating: Math.round(book.rating),
+    image: this.getImage(book.image?.url),
+  });
 
   getImage(imageUrl) {
     return `${this.apiBase}${imageUrl}`;
