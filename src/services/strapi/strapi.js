e40a7b0f9050ee9e1a54a82e08e7b4a6084a2908
Andrey <andrey.sergienko.dev@gmail.com>
2023-02-12 21:22:43 +0300

    Added method for getting current book data

--- src/services/strapi/strapi.js
+++ src/services/strapi/strapi.js
@@ -12,6 +12,17 @@ export class Strapi {
     return Promise.resolve(data.map(this.transformCard));
   }
 
+  async getBook(id) {
+    const response = await fetch(`${this.apiBase}/api/books/${id}`);
+    const book = await response.json();
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return Promise.resolve(this.transformCurrBook(book));
+  }
+
   getCategories = async () => {
     const response = await fetch(`${this.apiBase}/api/categories`);
     const data = await response.json();
@@ -28,6 +39,56 @@ export class Strapi {
     rating: Math.round(book.rating),
     image: this.getImage(book.image?.url),
   });
+
+  transformCurrBook = (book) => ({
+    ...book,
+    rating: Math.round(book.rating),
+    images: this.transformImagesUrl(book.images),
+    comments: this.transformComments(book.comments),
+  });
+
+  transformImagesUrl = (images) =>
+    images.map((image) => ({
+      url: this.getImage(image.url),
+    }));
+
+  formatDate = (date) => {
+    const years = [
+      'январь',
+      'февраль',
+      'март',
+      'апрель',
+      'май',
+      'июнь',
+      'июль',
+      'август',
+      'сентябрь',
+      'октябрь',
+      'ноябрь',
+      'декабрь',
+    ];
+
+    const localeDate = new Date(date).toLocaleDateString().split('/');
+
+    const day = localeDate[1];
+    const month = years[localeDate[0] - 1];
+    const year = localeDate[2];
+
+    return `${day} ${month} ${year}`;
+  };
+
+  transformComments = (comments) => {
+    if (!comments) return null;
+
+    return comments.map((comment) => ({
+      ...comment,
+      user: {
+        ...comment.user,
+        avatarUrl: comment.user.avatarUrl ? this.getImage(comment.user.avatarUrl) : null,
+      },
+      createdAt: this.formatDate(comment.createdAt),
+    }));
+  };
 
   getImage(imageUrl) {
     return `${this.apiBase}${imageUrl}`;
