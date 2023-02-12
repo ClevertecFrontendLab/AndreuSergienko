9d66ee02d74c85ca5ef08b6f5b55e7159620b97e
Andrey <andrey.sergienko.dev@gmail.com>
2023-02-12 21:21:19 +0300

    Removed extra constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,66 +1,3 @@
-export const CATEGORIES = [
-  {
-    name: 'Все книги',
-    path: 'all',
-  },
-  {
-    name: 'Бизнес-книги',
-    path: 'business',
-  },
-  {
-    name: 'Детективы',
-    path: 'detectives',
-  },
-  {
-    name: 'Детские книги',
-    path: 'children&books',
-  },
-  {
-    name: 'Зарубежная литература',
-    path: 'foreign&literature',
-  },
-  {
-    name: 'История',
-    path: 'history',
-  },
-  {
-    name: 'Классическая литература',
-    path: 'classic&literature',
-  },
-  {
-    name: 'Книги по психологии',
-    path: 'psycho&books',
-  },
-  {
-    name: 'Компьютерная литература',
-    path: 'computer&literature',
-  },
-  {
-    name: 'Культура и искусство',
-    path: 'culture&art',
-  },
-  {
-    name: 'Наука и образование',
-    path: 'science&education',
-  },
-  {
-    name: 'Публицистическая литература',
-    path: 'public&literature',
-  },
-  {
-    name: 'Справочники',
-    path: 'reference&books',
-  },
-  {
-    name: 'Фантастика',
-    path: 'fantasy',
-  },
-  {
-    name: 'Юмористическая литература',
-    path: 'humor&literature',
-  },
-];
-
 export const ACTIONS = {
   toggleBurger: 'TOGGLE_BURGER',
   toggleAccordion: 'TOGGLE_ACCORDION',
@@ -69,4 +6,5 @@ export const ACTIONS = {
   setCardsDisplay: 'SET_CARDS_DISPLAY',
   setBooks: 'SET_BOOKS',
   getCategories: 'GET_CATEGORIES',
+  setBook: 'SET_BOOK',
 };
