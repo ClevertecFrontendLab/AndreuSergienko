5397b17dcb9bf24eb0a7edb5fc633fc7ca9f0563
Andrey <andrey.sergienko.dev@gmail.com>
2023-02-12 21:22:58 +0300

    Added new reducer

---
+++ src/store/reducers/current-book-reducer.js
@@ -0,0 +1,16 @@
+import { ACTIONS } from 'constants';
+
+const initialState = {
+  book: null,
+};
+
+export const currBookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTIONS.setBook:
+      return { ...state, book: action.book };
+    default:
+      return state;
+  }
+};
+
+export const setBookAC = (book) => ({ type: ACTIONS.setBook, book });
