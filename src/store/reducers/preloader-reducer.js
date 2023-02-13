7f76df928593bac1c8ef70820450e71d94347a73
Andrey <andrey.sergienko.dev@gmail.com>
2023-02-13 22:39:07 +0300

    Added preloader reducer

---
+++ src/store/reducers/preloader-reducer.js
@@ -0,0 +1,16 @@
+import { ACTIONS } from 'constants';
+
+const initialState = {
+  isLoading: true,
+};
+
+export const preloaderReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTIONS.setLoading:
+      return { ...state, isLoading: action.preloaderView };
+    default:
+      return state;
+  }
+};
+
+export const setLoadingAC = (preloaderView) => ({ type: ACTIONS.setLoading, preloaderView });
