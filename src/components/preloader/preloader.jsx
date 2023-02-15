96c384fe6ec51c5d3ec367203afe59364ac9e73e
Andrey <andrey.sergienko.dev@gmail.com>
2023-02-15 23:51:52 +0300

    Removed extra chidlren using

--- src/components/preloader/preloader.jsx
+++ src/components/preloader/preloader.jsx
@@ -2,13 +2,13 @@ import { useSelector } from 'react-redux';
 
 import './preloader.css';
 
-export const Preloader = ({ children }) => {
+export const Preloader = () => {
   const isLoading = useSelector((state) => state.preloader.isLoading);
 
   if (isLoading) {
     return (
       <div className='spinnerWrapper' data-test-id='loader'>
-        <div className='spinner'>{children}</div>
+        <div className='spinner'>&nbsp;</div>
       </div>
     );
   }
