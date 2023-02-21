c9f25f7eae6cd388aba9bd389724c5b053d9e01b
Andrey <andrey.sergienko.dev@gmail.com>
2023-02-21 22:30:42 +0300

    Transformed error-reducer state

--- src/store/reducers/error-reducer.js
+++ src/store/reducers/error-reducer.js
@@ -1,16 +1,24 @@
 import { ACTIONS } from 'constants';
 
 const initialState = {
-  isError: false,
+  categoriesError: false,
+  booksError: false,
+  currBookError: false,
 };
 
 export const errorReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ACTIONS.setError:
-      return { ...state, isError: action.errorView };
+    case ACTIONS.setCategoriesError:
+      return { ...state, categoriesError: action.isError };
+    case ACTIONS.setBooksError:
+      return { ...state, booksError: action.isError };
+    case ACTIONS.setCurrBookError:
+      return { ...state, currBookError: action.isError };
     default:
       return state;
   }
 };
 
-export const setErrorAC = (errorView) => ({ type: ACTIONS.setError, errorView });
+export const setCategoriesErrorAC = (isError) => ({ type: ACTIONS.setCategoriesError, isError });
+export const setBooksErrorAC = (isError) => ({ type: ACTIONS.setBooksError, isError });
+export const setCurrBookErrorAC = (isError) => ({ type: ACTIONS.setCurrBookError, isError });
