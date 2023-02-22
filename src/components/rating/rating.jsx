9948b745097a7ea475c127613a45574ba498c4ab
Andrey <andrey.sergienko.dev@gmail.com>
2023-02-22 20:25:17 +0300

    Added unique keys using uuid

--- src/components/rating/rating.jsx
+++ src/components/rating/rating.jsx
@@ -1,20 +1,20 @@
-import { star, emptyStar } from 'assets/images/main-page/books';
+import { v4 as uuidv4 } from 'uuid';
+
+import { emptyStar, star } from 'assets/images/main-page/books';
 
 export const Rating = ({ length }) => {
   if (!length) return 'ещё нет оценок';
 
   const fullStarsLength = 5;
 
-  const starRating = new Array(length)
-    .fill(null)
-    .map((_, i) => <img key={Math.random() * length} src={star} alt='star' />);
+  const starRating = new Array(length).fill(null).map((_, i) => <img key={uuidv4()} src={star} alt='star' />);
 
   const missingStarsLength = 5 - starRating.length;
 
   if (starRating.length !== fullStarsLength) {
     const missingStars = new Array(missingStarsLength)
       .fill(null)
-      .map(() => <img key={Math.random() * length} src={emptyStar} alt='empty-star' />);
+      .map(() => <img src={emptyStar} key={uuidv4()} alt='empty-star' />);
     return starRating.concat(missingStars);
   }
 
