9f175dd8dd8cb64ecea69604eb5276155790f2ea
Andrey <andrey.sergienko.dev@gmail.com>
2023-02-22 20:47:12 +0300

    Added new switch case

--- src/store/reducers/search-reducer.js
+++ src/store/reducers/search-reducer.js
@@ -2,15 +2,19 @@ import { ACTIONS } from 'constants';
 
 const initialState = {
   isFull: false,
+  term: '',
 };
 
 export const searchReducer = (state = initialState, action) => {
   switch (action.type) {
     case ACTIONS.toggleSearch:
       return { ...state, isFull: action.searchView };
+    case ACTIONS.setTerm:
+      return { ...state, term: action.term };
     default:
       return state;
   }
 };
 
 export const toggleSearchAC = (searchView) => ({ type: ACTIONS.toggleSearch, searchView });
+export const setTermAC = (term) => ({ type: ACTIONS.setTerm, term });
