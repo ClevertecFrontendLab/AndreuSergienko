d907f71f09f93b08eff75506786f53d0f8741682
Andrey <andrey.sergienko.dev@gmail.com>
2023-02-28 22:50:30 +0300

    Added outlet

--- src/pages/auth/auth-page.jsx
+++ src/pages/auth/auth-page.jsx
@@ -1,25 +1,10 @@
-import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 
-import { useParams } from 'react-router-dom';
-
-import { FORM_FIELDS as formFields } from 'constants';
-
-import { AuthForm } from 'components';
 import styles from './auth-page.module.css';
 
-export const AuthPage = () => {
-  const [stepCounter, setStepCounter] = useState(1);
-
-  const addNextStep = () => {
-    setStepCounter((step) => step + 1);
-  };
-
-  const { authPath } = useParams();
-
-  return (
-    <div className={styles.canvas}>
-      <h3 className={styles.title}>Cleverland</h3>
-      <AuthForm {...formFields[authPath]} addNextStep={addNextStep} step={stepCounter} />
-    </div>
-  );
-};
+export const AuthPage = () => (
+  <div className={styles.canvas}>
+    <h3 className={styles.title}>Cleverland</h3>
+    <Outlet />
+  </div>
+);
