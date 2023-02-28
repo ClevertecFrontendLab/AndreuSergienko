585af57910f6cc6a8f3a9baa3ffd8acdc1553acb
Andrey <andrey.sergienko.dev@gmail.com>
2023-02-28 22:48:58 +0300

    Broked up forms constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -15,77 +15,86 @@ export const ACTIONS = {
   setCurrBookError: 'SET_CURR_BOOK_ERROR',
 };
 
-export const FORM_FIELDS = {
-  signUp: {
-    fields: [
-      {
-        inputs: [
-          {
-            type: 'text',
-            placeholder: 'Придумайте логин для входа',
-            tip: 'Используйте для логина латинский алфавит и цифры',
-          },
-          {
-            type: 'password',
-            placeholder: 'Пароль',
-            tip: 'Пароль не менее 8 символов, с заглавной буквой и цифрой',
-          },
-        ],
-        buttonContent: 'следующий шаг',
+export const SIGN_UP_FORM = {
+  fields: [
+    {
+      inputs: [
+        {
+          name: 'login',
+          type: 'text',
+          placeholder: 'Придумайте логин для входа',
+          tip: 'Используйте для логина латинский алфавит и цифры',
+        },
+        {
+          name: 'password',
+          type: 'password',
+          placeholder: 'Пароль',
+          tip: 'Пароль не менее 8 символов, с заглавной буквой и цифрой',
+        },
+      ],
+      button: {
+        content: 'следующий шаг',
       },
-      {
-        inputs: [
-          {
-            type: 'text',
-            placeholder: 'Имя',
-          },
-          {
-            type: 'text',
-            placeholder: 'Фамилия',
-          },
-        ],
-        buttonContent: 'последний шаг',
-      },
-      {
-        inputs: [
-          {
-            type: 'tel',
-            placeholder: 'Номер телефона',
-          },
-          {
-            type: 'text',
-            placeholder: 'E-mail',
-          },
-        ],
-        buttonContent: 'зарегистрироваться',
-      },
-    ],
-    link: {
-      content: 'войти',
-      path: '/signIn',
     },
-    title: 'Регистрация',
+    {
+      inputs: [
+        {
+          name: 'firstName',
+          type: 'text',
+          placeholder: 'Имя',
+          errorTip: 'Поле не должно быть пустым',
+        },
+        {
+          name: 'lastName',
+          type: 'text',
+          placeholder: 'Фамилия',
+          errorTip: 'Поле не должно быть пустым',
+        },
+      ],
+      button: {
+        content: 'последний шаг',
+      },
+    },
+    {
+      inputs: [
+        {
+          name: 'tel',
+          type: 'tel',
+          placeholder: 'Номер телефона',
+          tip: 'В формате +375 (xx) xxx-xx-xx',
+        },
+        {
+          name: 'email',
+          type: 'text',
+          placeholder: 'E-mail',
+        },
+      ],
+      button: {
+        content: 'зарегистрироваться',
+        type: 'submit',
+      },
+    },
+  ],
+  link: {
+    content: 'войти',
+    path: '/signIn',
   },
-  signIn: {
-    fields: [
-      {
-        inputs: [
-          {
-            type: 'text',
-            placeholder: 'Логин',
-          },
-          {
-            type: 'password',
-            placeholder: 'Пароль',
-          },
-        ],
-        buttonContent: 'вход',
-      },
-    ],
-    link: {
-      content: 'регистрация',
-      path: '/signUp',
+};
+
+export const SIGN_IN_FORM = {
+  inputs: [
+    {
+      type: 'text',
+      placeholder: 'Логин',
     },
-    title: 'Вход в личный кабинет',
+    {
+      type: 'password',
+      placeholder: 'Пароль',
+    },
+  ],
+  buttonContent: 'вход',
+  link: {
+    content: 'регистрация',
+    path: '/signUp',
   },
 };
