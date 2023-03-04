5a52f03783de345d264a438f2acb816f29ddd66d
Andrey <andrey.sergienko.dev@gmail.com>
2023-03-04 19:42:31 +0300

    Added sign up/in fetching

--- src/store/async-actions/async-actions.js
+++ src/store/async-actions/async-actions.js
@@ -1,5 +1,4 @@
 import { StrapiService } from 'services/strapi';
-
 import {
   setBooksAC,
   setBooksErrorAC,
@@ -7,12 +6,43 @@ import {
   setCategoriesErrorAC,
   setCurrBookAC,
   setCurrBookErrorAC,
-  setLoadingAC
+  setLoadingAC,
+  setRegResponseAC,
+  setRegStatusAC,
 } from 'store';
+
+export const fetchSignUp = (data) => async (dispatch) => {
+  dispatch(setLoadingAC(true));
+  try {
+    const response = await StrapiService.signUp(data);
+
+    dispatch(setRegResponseAC(response.data));
+    localStorage.setItem('jwt', response.data?.jwt);
+    dispatch(setRegStatusAC(response.statusText));
+  } catch (e) {
+    dispatch(setRegStatusAC(e.response?.data?.error?.name));
+  } finally {
+    dispatch(setLoadingAC(false));
+  }
+};
+
+export const fetchSignIn = (data, jwt) => async (dispatch) => {
+  try {
+    const response = await StrapiService.signIn(data, jwt);
+
+    console.log(response);
+
+    // dispatch(setCategoriesAC(response));
+  } catch (e) {
+    console.log(e);
+    // dispatch(setCategoriesErrorAC(true));
+  }
+};
 
 export const fetchCategories = () => async (dispatch) => {
   try {
     const response = await StrapiService.getCategories();
+
     dispatch(setCategoriesAC(response));
   } catch (e) {
     dispatch(setCategoriesErrorAC(true));
@@ -23,6 +53,7 @@ export const fetchBooks = () => async (dispatch) => {
   dispatch(setLoadingAC(true));
   try {
     const response = await StrapiService.getBooks();
+
     dispatch(setBooksAC(response));
   } catch (e) {
     dispatch(setBooksErrorAC(true));
@@ -35,6 +66,7 @@ export const fetchBook = (id) => async (dispatch) => {
   dispatch(setLoadingAC(true));
   try {
     const response = await StrapiService.getBook(id);
+
     dispatch(setCurrBookAC(response));
   } catch (e) {
     dispatch(setCurrBookErrorAC(true));
