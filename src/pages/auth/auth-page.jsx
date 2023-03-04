1358ca47cb918e6b19c9d6946760eb14782ca829
Andrey <andrey.sergienko.dev@gmail.com>
2023-03-04 19:40:23 +0300

    Added logic for displaying modals

--- src/pages/auth/auth-page.jsx
+++ src/pages/auth/auth-page.jsx
@@ -1,10 +1,24 @@
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
+import { FeedbackModal } from 'components';
+
+import { ERRORS, FAILED_FB, NON_UNIQUE_FB, SUCCESS_FB } from '../../constants';
 
 import styles from './auth-page.module.css';
 
-export const AuthPage = () => (
-  <div className={styles.canvas}>
-    <h3 className={styles.title}>Cleverland</h3>
-    <Outlet />
-  </div>
-);
+export const AuthPage = () => {
+  const regStatus = useSelector((state) => state.registration.status);
+
+  const uniqueModal = regStatus === ERRORS.uniques && <FeedbackModal {...NON_UNIQUE_FB} />;
+
+  const nonValidateModal = regStatus === ERRORS.nonValidate && <FeedbackModal {...FAILED_FB} />;
+
+  const success = regStatus === 'OK' && <FeedbackModal {...SUCCESS_FB} />;
+
+  return (
+    <div className={styles.canvas}>
+      <h3 className={styles.title}>Cleverland</h3>
+      {success || uniqueModal || nonValidateModal || <Outlet />}
+    </div>
+  );
+};
