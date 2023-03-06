b948385aaf7b22262340a0fab56548e04439bc01
Andrey <andrey.sergienko.dev@gmail.com>
2023-03-06 18:09:19 +0300

    Added redirect logic after succesful response

--- src/components/auth-forms/sign-in/sign-in-form.jsx
+++ src/components/auth-forms/sign-in/sign-in-form.jsx
@@ -1,6 +1,7 @@
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 import { fetchSignIn } from 'store';
 
 import styles from './sign-in-form.module.css';
@@ -8,10 +9,26 @@ import styles from './sign-in-form.module.css';
 export const SignInForm = () => {
   const { handleSubmit, register } = useForm();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    let ignore = false;
+
+    if (!ignore) {
+      if (authStatus === 200) {
+        navigate('/books/all');
+      }
+    }
+
+    return () => {
+      ignore = true;
+    };
+  }, [authStatus, navigate]);
 
   const onSubmit = (data) => {
     const jwt = localStorage.getItem('jwt');
-    
+
     dispatch(fetchSignIn(data, jwt));
   };
 
@@ -20,7 +37,13 @@ export const SignInForm = () => {
       <h4 className={styles.formTitle}>Вход в личный кабинет</h4>
       <div className={styles.fields}>
         <div className={styles.field}>
-          <input {...register('identifier')} className={styles.input} name='identifier' type='text' placeholder='Логин' />
+          <input
+            {...register('identifier')}
+            className={styles.input}
+            name='identifier'
+            type='text'
+            placeholder='Логин'
+          />
           {/* {input.tip ? <span className={styles.inputTip}>{input.tip}</span> : null} */}
         </div>
 
