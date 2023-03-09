76ec2e08654b23c393be2c2750b5e13ce90e4a12
Andrey <andrey.sergienko.dev@gmail.com>
2023-03-09 21:58:19 +0300

    Added some fixes in auth redirect mechanism

--- src/components/auth-forms/feedback-modal/feedback-modal.jsx
+++ src/components/auth-forms/feedback-modal/feedback-modal.jsx
@@ -1,6 +1,6 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { setRegStatusAC } from 'store';
+import { fetchSignIn, setAuthStatusAC, setRegStatusAC } from 'store';
 
 /* eslint-disable react/button-has-type */
 import styles from './feedaback-modal.module.css';
@@ -9,9 +9,18 @@ export const FeedbackModal = ({ title, errorText, link }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const authStatus = useSelector((state) => state.auth.status);
+  const authFormData = useSelector((state) => state.form.authData);
+
   const handleClick = () => {
     dispatch(setRegStatusAC(''));
-    navigate(link.path);
+    dispatch(setAuthStatusAC(''));
+    if (link.path) {
+      navigate(link.path);
+    }
+    if (authStatus) {
+      dispatch(fetchSignIn(authFormData));
+    }
   };
 
   return (
