3c06f720088043c428cf87d533fffba4a4675c92
Andrey <andrey.sergienko.dev@gmail.com>
2023-03-09 22:01:12 +0300

    Added new modal with bad response

--- src/pages/auth/auth-page.jsx
+++ src/pages/auth/auth-page.jsx
@@ -2,23 +2,26 @@ import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import { FeedbackModal } from 'components';
 
-import { ERRORS, FAILED_FB, NON_UNIQUE_FB, SUCCESS_FB } from '../../constants';
+import { ERRORS, FAILED_FB, NON_UNIQUE_FB, SUCCESS_FB, FAILED_LOGIN_FB } from '../../constants';
 
 import styles from './auth-page.module.css';
 
 export const AuthPage = () => {
   const regStatus = useSelector((state) => state.registration.status);
+  const authStatus = useSelector((state) => state.auth.status);
 
   const uniqueModal = regStatus === ERRORS.uniques && <FeedbackModal {...NON_UNIQUE_FB} />;
 
   const nonValidateModal = regStatus === ERRORS.nonValidate && <FeedbackModal {...FAILED_FB} />;
+
+  const failedLogin = authStatus && authStatus !== 400 && authStatus !== 200 && <FeedbackModal {...FAILED_LOGIN_FB} />;
 
   const success = regStatus === 'OK' && <FeedbackModal {...SUCCESS_FB} />;
 
   return (
     <div className={styles.canvas}>
       <h3 className={styles.title}>Cleverland</h3>
-      {success || uniqueModal || nonValidateModal || <Outlet />}
+      {success || uniqueModal || nonValidateModal || failedLogin || <Outlet />}
     </div>
   );
 };
